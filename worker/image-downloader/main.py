a359013a9516c99d331c0883e42dd7b424b2c109
fix-a-lot <fixalot@outlook.kr>
2026-02-11 10:52:30 +0900

    feat: 이미지 다운로드 코드 개선

--- worker/image-downloader/main.py
+++ worker/image-downloader/main.py
@@ -1,28 +1,30 @@
 import os
 import requests  # request img from web
 import shutil  # save img locally
-
 from target_list import download_target_list
 
 dependsOnUrl = False # True
 extension = ''
 download_target_list
 
-parentDir = 'c:/dev/test/'
+parentDir = './output/'
 if not os.path.isdir(parentDir):
     os.mkdir(parentDir)
 
-str = ''
+preview_content = ''
 for target in download_target_list:
     url = target['url']
-    if (dependsOnUrl):
-        extension = url[url.rfind('.'): len(url)]
+    if dependsOnUrl:
+        extension = url[url.rfind('.'):]
+    
     fileName = target['name'] + extension
-    str += 'url = ' + url + '\n'
-    str += 'fileLocation = ' + parentDir + fileName + '\n\n'
+    fileLocation = os.path.join(parentDir, fileName)
+    preview_content += f"url = {url}\n"
+    preview_content += f"fileLocation = {fileLocation}\n\n"
 
-with open(parentDir + 'preview.txt', 'w') as f:
-    f.write(str)
+preview_path = os.path.join(parentDir, 'preview.txt')
+with open(preview_path, 'w', encoding='utf-8') as f:
+    f.write(preview_content)
 
 for target in download_target_list:
     url = target['url']
@@ -30,10 +32,14 @@ for target in download_target_list:
         extension = url[url.rfind('.'): len(url)]
     fileName = target['name'] + extension
     destination = parentDir + fileName
-    res = requests.get(url, stream=True)
-    if res.status_code == 200:
-        with open(destination, 'wb') as f:
-            shutil.copyfileobj(res.raw, f)
-        print('Image sucessfully Downloaded: ', destination)
-    else:
-        print('Image Couldn\'t be retrieved')
+    
+    try:
+        with requests.get(url, stream=True) as res:
+            if res.status_code == 200:
+                with open(destination, 'wb') as f:
+                    shutil.copyfileobj(res.raw, f)
+                print(f"Image successfully Downloaded: {destination}")
+            else:
+                print(f"Image Couldn't be retrieved. url: {url}, status_code: {res.status_code}")
+    except Exception as e:
+        print(f"Error occurred while downloading {url}: {e}")
